a18ec604e4572efa5890354974b94ec060272f74
Home <home@Homes-MBP.broadband>
2020-09-30 13:20:32 +0100

    Updates

---
+++ precache-manifest.2469fe6755299e80b5187a15191c72b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87a338664b8e13c81193de189a8f458",
+    "url": "/oli-gra/camden-covid.git/index.html"
+  },
+  {
+    "revision": "d789f2045398338c0769",
+    "url": "/oli-gra/camden-covid.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d782bea1cbce244830a6",
+    "url": "/oli-gra/camden-covid.git/static/js/2.98657f93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oli-gra/camden-covid.git/static/js/2.98657f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d789f2045398338c0769",
+    "url": "/oli-gra/camden-covid.git/static/js/main.d19aa11b.chunk.js"
+  },
+  {
+    "revision": "e0880e2cb9eefde67afb",
+    "url": "/oli-gra/camden-covid.git/static/js/runtime-main.59f8a6db.js"
+  }
+]);
